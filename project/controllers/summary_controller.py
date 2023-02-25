8e95b4541a3e1d5b0422ae9238f17bc644d394a0
naka-c1024 <78196739+naka-c1024@users.noreply.github.com>
2023-02-25 13:44:57 +0900

    update: 簡単なdemo (#13)

--- project/controllers/summary_controller.py
+++ project/controllers/summary_controller.py
@@ -1,4 +1,24 @@
 from flask import Flask, flash, redirect, render_template, request, session
+import wikipedia
+
+# リダイレクト元：map.html
+# 地域名をgetで取得
 
 def summary():
-    return render_template("summary.html")
+    region_name = request.args.get('region')
+
+    # キーワードで検索
+    wikipedia.set_lang("ja")
+    search_response = wikipedia.search(region_name)
+
+    #検索結果を表示
+    # print('キーワード:'+ str(region_name) + 'での検索結果は' + str(len(search_response)) + '件です。')
+    # print(search_response)
+    #検索結果のページ内容を表示
+    page_data = wikipedia.page(search_response[0])
+    # print(page_data.content) # コンテンツ全て
+    # print(page_data.title) # タイトル
+    # print(page_data.summary) # 要約
+    # print(page_data.html()) # html
+
+    return render_template("summary.html", wiki_summary=page_data.summary)
