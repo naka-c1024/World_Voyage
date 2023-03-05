a94048e0004dec77edab3996f9ef2feb60cd573d
sae-eg <118035666+sae-eg@users.noreply.github.com>
2023-03-05 17:01:19 +0900

    summaryへの画像表示・timilineの目次抽出 (#36)
    * サムネイル取得＆エラーno表示
    
    * summaryへの画像表示・timilineの目次抽出

--- project/app.py
+++ project/app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded, Trueにすると、テンプレートが変更されたときに再読み込みする。
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Flaskのデフォルトである(デジタル署名された)cookie内に格納するのではなく、ローカルファイルシステム(ディスク)に格納するようにFlaskを構成
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # app.add_url_rule("/", view_func=index)
 
 @app.route("/")
