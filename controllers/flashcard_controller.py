fa53ab11832579b21deaaf06421f1e1ad654ac6c
sae-eg <118035666+sae-eg@users.noreply.github.com>
2023-03-14 12:11:29 +0900

    flashcard.htmlとcontrollerの追加 (#77)
    * index.htmlの写真・説明文追加
    
    * flashcard.htmlとcontrollerの追加

---
+++ controllers/flashcard_controller.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+import sqlite3
+
+def flashcard():
+    conn = sqlite3.connect("globe.db")
+    c = conn.cursor()
+    # c.execute("INSERT INTO flashcards (region_name, title, detail) VALUES(?, ?, ?)")
+    flashcards = c.execute("SELECT * FROM flashcards")
+    return render_template("flashcard.html", flashcards=flashcards)
+
+# flashcardは、country_infoから登録できるのか、どこから登録？
